687654c2ad486ad879271b4ee906deb936656461
noracsus <47579527+noracsus@users.noreply.github.com>
2019-02-18 16:57:41 -0800

    Update k-lib.c

--- k-lib.c
+++ k-lib.c
@@ -10,11 +10,11 @@ void Bzero(char *p, int bytes) {
 }
 
 int QisEmpty(q_t *p) { // return 1 if empty, else 0
-   return !p->tail;
+   ...
 }
 
 int QisFull(q_t *p) { // return 1 if full, else 0
-   return p->tail == Q_SIZE; 
+   ...
 }
 
 // dequeue, 1st # in queue; if queue empty, return -1
@@ -23,17 +23,12 @@ int DeQ(q_t *p) { // return -1 if q[] is empty
    int ..., ...
 
    if(QisEmpty(p)) {
-      cons_printf("Panic: queue is full, cannot EnQ!\n");
-      return -1; //breakpoint ();
+      ...
    }
 
-   to_return = p->q[0];
-   p->tail--;
-   for(i=0; i<p->tail; i++)p->q[i] = p->q[i+1];
-   
-   //for(i=tail; i<Q_SIZE; i++)p->q[i] = NONE;
-   p->q[p->tail] = -1;
-   return to_return;
+   ...
+   ...
+   ...
    ...
    ...
 }
@@ -42,11 +37,9 @@ int DeQ(q_t *p) { // return -1 if q[] is empty
 void EnQ(int to_add, q_t *p) {
    if(QisFull(p)) {
       cons_printf("Panic: queue is full, cannot EnQ!\n");
-      return; //breakpoint();
+      ...
    }
 
-   p->q[tail] = to_add;
-   p->tail++;
-   //p->q[tail] = NONE;  good idea 
+   ...
+   ...
 }
-
