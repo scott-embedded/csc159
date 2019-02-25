6bb71db12512d2faf1039b52eb2d8ab962b561c4
noracsus <47579527+noracsus@users.noreply.github.com>
2019-02-25 14:29:02 -0800

    Update sys-call.c

--- sys-call.c
+++ sys-call.c
@@ -28,13 +28,13 @@ void ShowCharCall(int row, int col, char ch) {
 }
 
 void SleepCall(int centi_sec) {  // # of 1/100 of a second to sleep
-   for(i=0; i<centi_sec/2; i++) asm("inb $0x80");
+  // for(i=0; i<centi_sec/2; i++) asm("inb $0x80");
 
 
       asm("movl %0, %%eax;
            int %1"     // after, copy eax to variable 'pid'
-       :         // output
-       : "g" (centi_sec), "g" (SLEEP_CALL)  // input
-       : "eax"              // used registers
+           :         // output
+           : "g" (centi_sec), "g" (SLEEP_CALL)  // input
+           : "eax"              // used registers
    ); //for now, but please adjust this into more asm 
 }
